08f756587659a51df1ce986d4d9b386a0ee2afe3
eric.tuchscherer <eric.tuchscherer@classmates.com>
2014-06-06 15:25:32 -0700

    Adding login functionality

--- app/main.js
+++ app/main.js
@@ -4,4 +4,90 @@ App.Router.map(function() {
 	this.resource('home', { path: '/' });
 	this.resource('about', { path: '/about' });
 	this.resource('contact', { path: '/contact' });
+	this.resource('login', { path: '/login' });
+
+});
+
+App.LoginRoute = Ember.Route.extend({
+	setupController: function(controller, context) {
+		controller.reset();
+	}
+});
+
+App.AuthenticatedRoute = Ember.Route.extend({
+	beforeModel: function(transition) {
+		if (!this.controllerFor('login').get('token')) {
+			this.redirectToLogin(transition);
+		}
+	},
+
+	getJSONWithToken: function(url) {
+		return $.getJSON(url, { token: this.controllerFor('login').get('token') });
+	},
+
+	redirectToLogin: function(transition) {
+		var loginController = this.controllerFor('login');
+		loginController.set('attemptedTransition', transition);
+
+		this.transitionTo('login');		
+	},
+
+	actions: {
+		error: function(reason, transition) {
+			if (reason.status === 403) {
+				this.loginRequired();
+				this.redirectToLogin(transition);
+			} else {
+				console.error('something went wrong');
+			}
+		}
+	}
+});
+
+App.ContactRoute = App.AuthenticatedRoute.extend({
+	model: function() {
+		return this.getJSONWithToken('/data.json');
+	}
+});
+
+App.LoginController = Ember.Controller.extend({
+
+	reset: function() {
+		this.setProperties({
+			username: "",
+			password: "",
+			errorMessage: ""
+		});
+	},
+
+	token: localStorage.token,
+
+	tokenChanged: function() {
+		localStorage.token = this.get('token');
+	}.observes('token'),
+
+	actions: {
+		login: function() {
+			var self = this,
+				data = this.getProperties('username', 'password');
+
+			self.set('errorMessage', null);
+			Ember.$.post('/auth.json', data).then(function(r) {
+
+				var attemptedTransition = self.get('attemptedTransition');
+				self.set('errorMessage', r.message);
+
+				if (r.success) {
+					if (attemptedTransition) {
+						attemptedTransition.retry();
+						self.set('attemptedTransition', null);
+					} else {	// transition to contact by default
+						self.transitionToRoute('contact');	
+					}
+					self.set('token', r.token);
+
+				}
+			});
+		}
+	}
 });
--- index.js
+++ index.js
@@ -1,8 +1,61 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
+app.use(bodyParser());
+
+var currentToken;
+
+var contacts = [
+	{
+		name: 'Steve',
+		number: 2013330941
+	}
+];
+
+
+app.get('/data.json', function(req, res) {
+	if (validTokenProvided(req, res)) {
+		res.send(contacts);
+	}
+});
 
 app.use('/', express.static(__dirname + '/app'));
 app.use('/js', express.static(__dirname + '/bower_components'));
+
+app.post('/auth.json', function(req, res) {
+
+  var body = req.body,
+      username = body.username,
+      password = body.password;
+
+  if (username == 'ember' && password == 'casts') {
+    // Generate and save the token (forgotten upon server restart).
+    currentToken = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    res.send({
+      success: true,
+      token: currentToken
+    });
+  } else {
+    res.send({
+      success: false,
+      message: 'Invalid username/password'
+    });
+  }
+});
+
+function validTokenProvided(req, res) {
+
+  // Check POST, GET, and headers for supplied token.
+  var userToken = req.body.token || req.param('token') || req.headers.token;
+
+  if (!currentToken || userToken != currentToken) {
+    res.send(403, { error: 'Invalid token. You provided: ' + userToken });
+    return false;
+  }
+
+  return true;
+}
 
 app.listen(3000);
 console.log('Server is running…');
